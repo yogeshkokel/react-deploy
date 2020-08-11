2c4862b7a365d9f47ed7f14525faaced5efaa693
yogeshk <yogesh@binarynumbers.io>
2020-08-11 19:39:30 +0530

    Updates

---
+++ precache-manifest.37af51f0b19b7f781cc510abdd3b4bf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62a6b5254ea16eb09475b46d75eb7f7",
+    "url": "/github.io/react-deploy/index.html"
+  },
+  {
+    "revision": "8f0cddc03a11aeb441d0",
+    "url": "/github.io/react-deploy/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "85aea6ba848c2593903c",
+    "url": "/github.io/react-deploy/static/css/main.b170d7de.chunk.css"
+  },
+  {
+    "revision": "8f0cddc03a11aeb441d0",
+    "url": "/github.io/react-deploy/static/js/2.76ad8a19.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/github.io/react-deploy/static/js/2.76ad8a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85aea6ba848c2593903c",
+    "url": "/github.io/react-deploy/static/js/main.e77e5efe.chunk.js"
+  },
+  {
+    "revision": "34ea6899a0b549cc2726",
+    "url": "/github.io/react-deploy/static/js/runtime-main.2aa1524f.js"
+  }
+]);
